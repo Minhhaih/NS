8ed14138437f2e560cd2a1226bc9f078398fba5c
Thimo <thimo@vodafonethuis.nl>
2016-11-06 15:32:36 +0100

    Knopjes erbij!

--- ns_info.py
+++ ns_info.py
@@ -13,26 +13,39 @@ from PIL import Image, ImageTk
 # Build nummer updater
 build = 0
 with open("build.txt", "r") as f:
-	contents = f.read()
-	f.close()
+    contents = f.read()
+    f.close()
 
 build = int(contents) + 1
 
 with open("build.txt", "w") as f:
-	f.write(str(build))
-	f.close()
-	
+    f.write(str(build))
+    f.close()
+
 # Grafische Gebruikers Interface details:
-h1_font = ("Helvetica", 20, "bold")
-front = "#002d72" # Geel
-back = "#fcc917" # Blauw
+top_text = ("Helvetica", 20, "bold")
+UIBackground = "#fcc917" # Geel
+front_text = "#002d72"  # Blauw
+back_text = "#fcc917"  # Blauw
+front_knop = "#ffffff" # Wit
+back_knop = "#002d72" # Blauw
+knop = font=("Helvetica", 10)
 
 # Grafische Gebruikers Interface:
 UI = Tk()
-UI.configure(height = 500, width = 1000, background=back)
+UI.configure(height=250, width=635, background=UIBackground)
 UI.title('NS Automaat')
 
-Label(UI,text='Build number: '+ str(build), foreground=front, background=back).place(x=0,y=0)
-Label(UI,text='Welkom bij NS', font=h1_font, foreground =front, background=back).place(x=200,y=30)	
+# Labels
+Label(UI, text='Build versie: ' + str(build), foreground=front_text, background=back_text).place(x=0, y=0)
+Label(UI, text='Welkom bij NS', font=top_text, foreground=front_text, background=back_text).place(x=200, y=30)
+Label(UI, text='Door: Thimo, Koen, Remon en Edo', foreground=front_text, background=back_text).place(x=215, y=215)
+
+# Knoppen
+KnopAMS = Button(UI, text='Ik wil naar \nAmsterdam', foreground=front_knop, background=back_knop, font=knop).place(x=45, y=100)
+KnopKaart = Button(UI, text='Kopen \nlos kaartje', foreground=front_knop, background=back_knop, font=knop).place(x=150, y=100)
+KnopOV = Button(UI, text='Kopen \nOV-Chipkaart', foreground=front_knop, background=back_knop, font=knop).place(x=250, y=100)
+KnopBuitenland = Button(UI, text='Ik wil naar \nhet buitenland', foreground=front_knop, background=back_knop, font=knop).place(x=370, y=100)
+KnopVertrek = Button(UI, text='Huidige \nvertrektijden', foreground=front_knop, background=back_knop, font=knop).place(x=490, y=100)
 
 UI.mainloop()
